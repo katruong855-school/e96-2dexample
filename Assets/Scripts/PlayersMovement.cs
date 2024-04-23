74553e37f7878b9d82147b4106e09de064eb0b5c
katruong855-school <165821103+katruong855-school@users.noreply.github.com>
2024-04-22 21:25:24 -0700

    Player Flips and Added to Level
    Bibingka now FLIPS!!!
    - sprite flips according to direction when walking
    - level platforms added to
    - sorting layers added

--- Assets/Scripts/PlayersMovement.cs
+++ Assets/Scripts/PlayersMovement.cs
@@ -16,12 +16,13 @@ public class PlayersMovement : MonoBehaviour
     private PlayerInput _inputActionReference; //reference of generated C# script from the input
     private Rigidbody2D _playersRigidBody; //reference to players rigid body
     private bool isGrounded = true;
+    private bool facingLeft = true;
 
     
     // Start is called before the first frame update
     private void Start()
     {
-        _playersRigidBody ??= GetComponent<Rigidbody2D>();
+        _playersRigidBody = GetComponent<Rigidbody2D>();
         _inputActionReference = new PlayerInput();
         //enable input actions
         _inputActionReference.Enable();
@@ -47,6 +48,12 @@ public class PlayersMovement : MonoBehaviour
         //use the player's rigidbody to move it
         _playersRigidBody.velocity =
             new Vector2(_playersMovementDirection * PlayersMovementSpeed, _playersRigidBody.velocity.y);
+
+        if(_playersMovementDirection > 0 && facingLeft){
+            Flip();
+        }else if(_playersMovementDirection < 0 && !facingLeft){
+            Flip();
+        }
         
         
     }
@@ -60,6 +67,14 @@ public class PlayersMovement : MonoBehaviour
             isGrounded = false;
         }
         
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingLeft = !facingLeft;
     }
 }
 
